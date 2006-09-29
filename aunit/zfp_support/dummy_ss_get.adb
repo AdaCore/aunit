0f5b47662054c24fc4c838979ad94e634e104e4d
Jerome Lambourg <lambourg@adacore.com>
2006-09-29 10:23:45 +0000

    Removed from aunit directory. Part of EC16-012

--- aunit/zfp_support/dummy_ss_get.adb
+++
@@ -1,4 +0,0 @@
-function Dummy_SS_Get return System.Address is
-begin
-   return System.Null_Address;
-end Dummy_SS_Get;
--- aunit/zfp_support/dummy_ss_get.ads
+++
@@ -1,3 +0,0 @@
-with System;
-function Dummy_SS_Get return System.Address;
-pragma Export (C, Dummy_SS_Get, "__gnat_get_secondary_stack");
