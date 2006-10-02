b4015fd94740306b65b16bfe0c8707321603cea9
Jerome Lambourg <lambourg@adacore.com>
2006-10-02 14:16:38 +0000

    Reorganisation of run-time dependant files to features dependant files. Part of EC16-012

---
+++ aunit/framework/aunit_framework-tests-test_cases-run_routine.adb
@@ -0,0 +1,80 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--    A U N I T . T E S T S . T E S T _ C A S E S . R U N _ R O U T I N E   --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2000-2006, AdaCore                   --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GNAT is maintained by AdaCore (http://www.adacore.com)                   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Exceptions; use Ada.Exceptions;
+
+separate (AUnit_Framework.Tests.Test_Cases)
+
+--  Version for run-time libraries that support exception handling
+procedure Run_Routine
+  (Test    : access Test_Case'Class;
+   Subtest : Routine_Spec;
+   R       : access Result) is
+
+   use Message_Lists;
+   Unexpected_Exception : Boolean := False;
+begin
+
+   --  Reset failure list to capture failed assertions for one routine
+
+   Clear (Test.Failures);
+
+   --  Run test routine
+
+   Set_Up (Test.all);
+
+   begin
+      Subtest.Routine.all (Test.all);
+   exception
+      when Assertion_Error =>
+         null;
+
+      when E : others =>
+         Unexpected_Exception := True;
+         Add_Error (R.all,
+                    Name (Test.all),
+                    Subtest.Routine_Name,
+                    Format_Message (Exception_Name (E)));
+   end;
+
+   Tear_Down (Test.all);
+
+   if not Unexpected_Exception and then Is_Empty (Test.Failures) then
+      Add_Success (R.all, Name (Test.all), Subtest.Routine_Name);
+   else
+      declare
+         C : Cursor := First (Test.Failures);
+      begin
+         while Has_Element (C) loop
+            Add_Failure (R.all,
+                         Name (Test.all),
+                         Subtest.Routine_Name,
+                         Element (C));
+            Next (C);
+         end loop;
+      end;
+   end if;
+end Run_Routine;
---
+++ aunit/framework/noexception/aunit_framework-tests-test_cases-assertions.adb
@@ -0,0 +1,51 @@
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--                     A U N I T . A S S E R T I O N S                      --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2000-2006, AdaCore                   --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GNAT is maintained by AdaCore (http://www.adacore.com)                   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Version for run-time libraries that do not support exception handling
+
+package body AUnit_Framework.Tests.Test_Cases.Assertions is
+
+   ------------
+   -- Assert --
+   ------------
+
+   procedure Assert
+     (Condition : Boolean;
+      Message   : String) is
+   begin
+      if not Condition then
+         Register_Failure (Current_Test_Case, Message);
+      end if;
+   end Assert;
+
+   function Assert
+     (Condition : Boolean;
+      Message   : String) return Boolean is
+   begin
+      Assert (Condition, Message);
+      return Condition;
+   end Assert;
+
+end AUnit_Framework.Tests.Test_Cases.Assertions;
