1dc87292918e5c7f045f0c09f5139d520d73c2e3
Jerome Lambourg <lambourg@adacore.com>
2007-06-11 16:08:43 +0000

    Update copyright notice.

--- aunit/framework/aunit_framework-tests-test_cases.adb
+++ aunit/framework/aunit_framework-tests-test_cases.adb
@@ -7,7 +7,7 @@
 --                                 B o d y                                  --
 --                                                                          --
 --                                                                          --
---                       Copyright (C) 2000-2006, AdaCore                   --
+--                       Copyright (C) 2000-2007, AdaCore                   --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
