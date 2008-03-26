1b1b9609b040653e22cf2d8a4e2907c8e2018bdb
Jerome Lambourg <lambourg@adacore.com>
2008-03-26 10:30:01 +0000

    Renamed text_reporter to reporters as this directory will contain also the  XML reporter. Part of H201-024

---
+++ aunit/reporters/aunit_framework-test_results-text_reporter.ads
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--         A U N I T _ F R A M E W O R K . T E S T _ R E S U L T S .        --
+--                       T E X T _ R E P O R T E R                          --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2000-2006, AdaCore                   --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GNAT is maintained by AdaCore (http://www.adacore.com)                   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Very simple reporter to console
+generic
+package AUnit_Framework.Test_Results.Text_Reporter is
+   procedure Report (R : in out Result);
+end AUnit_Framework.Test_Results.Text_Reporter;
