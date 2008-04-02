34ffd4358b4afee141d025c93c83fdc6fcc5edd2
Jerome Lambourg <lambourg@adacore.com>
2008-04-02 10:56:26 +0000

    Adapt the doinstall.bat script to the new compilation/installation schema. Part of H201-024

--- doinstall.bat
+++ doinstall.bat
@@ -8,11 +8,11 @@ IF NOT '%I%' == '' (
   GOTO END
 )
 
+SETLOCAL
+
 REM === reset of the used variables
-SET GNATROOT=
+SET GPRBUILDROOT=
 SET GNAT=
-SET SUPPORT_EXCEPTION=
-SET SUPPORT_CALENDAR=
 SET INSTALL=
 SET RUNTIME=
 SET TOOL_PREFIX=
@@ -20,165 +20,190 @@ SET TOOL_PREFIX=
 REM ===== CONFIGURATION PHASE ===========
 
 REM Retrieving the GNAT compiler from registry
-SET GNATROOT=
-ECHO.>GNATROOT.TXT
-FOR /F "skip=9 tokens=1,3" %%A IN ('REG QUERY "HKLM\Software\Ada Core Technologies" /S') DO (
+SET GPRBUILDROOT=
+REG QUERY "HKLM\Software\Ada Core Technologies\GPRBUILD" /v ROOT > REGS.TXT | GOTO MISSINGGPRBUILD
+FOR /F "skip=2 tokens=1,3" %%A IN (REGS.TXT) DO (
   if %%A == ROOT (
-    ECHO %%B >> GNATROOT.TXT
-    SET GNATROOT=%%B
+    SET GPRBUILDROOT=%%B
   )
 )
+DEL REGS.TXT
 
+CLS
 ECHO.
-ECHO *** COMPILER ***
 ECHO.
-:SELECT_COMPILER
+ECHO.
+ECHO **************************
+ECHO *** PREPARING TO SETUP ***
+ECHO **************************
+ECHO.
+:SELECT_GPRBUILD
 
 SET CHOICE=
-IF '!GNATROOT!' == '' (
-  ECHO Please specify the full path to gnat.exe:
+IF '!GPRBUILDROOT!' == '' (
+  ECHO Please specify the full path to gprbuild.exe:
   SET /P CHOICE=
   IF '!CHOICE!' == '' (
-    ECHO *** Error ! Please enter a valid path for gnat.exe
-    GOTO SELECT_COMPILER
+    ECHO *** Error ! Please enter a valid path for gprbuild.exe
+    GOTO SELECT_GPRBUILD
   )
 
-  GOTO ANALYSE_PATH
+)
+
+:SELECT_TARGET
+SET /A I = 0
+SET TARGETS=
+ECHO.>TARGETSCHOICE.TXT
+for /F "skip=1" %%A IN ('%GPRBUILDROOT%\bin\gprconfig --show-targets') DO (
+  ECHO %%A >> TARGETSCHOICE.TXT
+  SET /A I=!I!+1
+  SET LAST_TARGET=%%A
+)
+
+IF '!I!' LEQ '1' (
+  ECHO %LAST_TARGET% > TARGETS.TXT
 ) ELSE (
-  ECHO Please specify the compiler you want to use
-  ECHO ** Select one from the list, or directly enter gnat.exe's full path:
-  SET /A I = O
-  SET PREV =
-  FOR /F %%A IN ('SORT GNATROOT.TXT') DO (
-    IF '%%A' NEQ '!PREV!' (
-      SET PREV=%%A
-      FOR /F %%B IN ('DIR /B %%A\bin\*gnat.exe') DO (
-        SET /A I=!I!+1
-        ECHO !I!- %%A\bin\%%B
+  ECHO.>TARGETS.TXT
+  SET ONESELECTED=
+:SELECT_TARGETS
+  SET /A J=O
+  SET OPTIONS=
+  CLS
+  ECHO *** Select the targets you want AUnit to be compiled for.
+  FOR /F %%A IN (TARGETSCHOICE.TXT) DO (
+    SET FOUND=0
+    FOR /F %%B IN (TARGETS.TXT) DO (
+      IF '%%A' == '%%B' (
+        SET FOUND=1
+      )
+    )
+    SET /A J=!J!+1
+    IF '!FOUND!' == '0' (
+      SET OPTIONS=!OPTIONS!!J!
+      SET NUM= ^(!J!^)
+    ) ELSE (
+      SET NUM=*^(!J!^)
+    )
+    ECHO !NUM! %%A
+  )
+  SET CHOICE=
+  IF '!ONESELECTED!' == '' (
+    SET /P CHOICE="Enter a target number: "
+  ) ELSE (
+    SET /P CHOICE="Enter a target number, or <enter> to continue the setup: "
+  )
+  IF '!CHOICE!' LEQ '' (
+    IF '!ONESELECTED!' == '' (
+      GOTO SELECT_TARGETS
+    ) ELSE (
+      GOTO END_SELECT_TARGETS
+    )
+  )
+  SET /A J=0
+  FOR /F %%A IN (TARGETSCHOICE.TXT) DO (
+    SET /A J=!J!+1
+    SET FOUND=0
+    FOR /F %%B IN (TARGETS.TXT) DO (
+      IF '%%A' == '%%B' (
+        SET FOUND=1
+      )
+    )
+    IF !FOUND! LEQ '0' (
+      IF '!J!' == '!CHOICE!' (
+        SET /A I=!I!-1
+        ECHO %%A >> TARGETS.TXT
+        SET ONESELECTED=1
       )
     )
   )
-  SET /P CHOICE=
-  IF '!CHOICE!' == '' (
-    ECHO *** Error ! Please select a compiler from the list, or enter a valid path
-    GOTO SELECT_COMPILER
+  IF '!I!' NEQ '0' (
+    GOTO SELECT_TARGETS
   )
 )
-SET GNAT=
-SET GNATROOT=
+DEL TARGETSCHOICE.TXT
 
-IF '!CHOICE!' LEQ '!I!' (
-  SET /A I=0
-  SET PREV =
-  FOR /F %%A IN ('SORT GNATROOT.TXT') DO (
-    IF '%%A' NEQ '!PREV!' (
-      SET PREV=%%A
-      FOR /F %%B IN ('DIR /B %%A\bin\*gnat.exe') DO (
-        SET /A I=!I!+1
-        IF !I! == !CHOICE! (
-          SET GNAT=%%A\bin\%%B
-          SET GNATROOT=%%A
-          GOTO COMPILER_END
-        )
-      )
+:END_SELECT_TARGETS
+
+ECHO ECHO OFF>                   RUN.BAT
+
+FOR /F %%A IN (TARGETS.TXT) DO (
+  CALL :SELECT_RUNTIME %%A RUN.BAT
+)
+DEL TARGETS.TXT
+
+ECHO GOTO END>>                  RUN.BAT
+ECHO ^:ERROR>>                   RUN.BAT
+ECHO ECHO *** BUILD FAILED ***>> RUN.BAT
+ECHo ECHO.>>                     RUN.BAT
+ECHO EXIT /B 1>>                 RUN.BAT
+ECHO ^:END>>                     RUN.BAT
+
+GOTO END_SELECT_RUNTIME
+
+:SELECT_RUNTIME
+  SET TARGET=%1
+  SET SCRIPT=%2
+  ECHO.>OPTS.TXT
+:START_SELECT_RUNTIME
+  CLS
+  ECHO * Selected Platform *
+  ECHO !TARGET!
+  ECHO.
+  ECHO * Runtime^(s^)      *
+  TYPE OPTS.TXT
+  ECHO.
+  ECHO Please select the compiler you want to use for this platform:
+  ECHO.
+  %GPRBUILDROOT%\bin\gprconfig --target=!TARGET! --config=Ada,* --config=C -o !TARGET!.cgpr || GOTO ERROR
+  ECHO.>RTS.TXT
+  FOR /F "tokens=1,4" %%B IN (!TARGET!.cgpr) DO (
+    IF "%%B" == "Binder'Required_Switches" (
+      ECHO %%C>>RTS.TXT
     )
   )
-)
+  FINDSTR RTS= RTS.TXT>RTSOPT.TXT || ECHO ^(^"--RTS=full^"^)^;>RTSOPT.TXT
+  DEL RTS.TXT
+  for /F "delims=^)" %%B in (RTSOPT.TXT) DO (SET OPT=%%B)
+  SET RUNTIME=!OPT:~8,-1!
+  ECHO !RUNTIME!>>OPTS.TXT
+  DEL RTSOPT.TXT
+  RENAME !TARGET!.cgpr !TARGET!-!RUNTIME!.cgpr
+  IF "!TARGET!" == "pentium-mingw32msv" (
+    SET PLATFORM="native"
+  ) ELSE (
+    SET PLATFORM=!TARGET!
+  )
+  ECHO %GPRBUILDROOT%\bin\gprbuild --config=!TARGET!-!RUNTIME!.cgpr -XRUNTIME=!RUNTIME! -XPLATFORM=!PLATFORM! -p -f -Paunit/aunit_build.gpr ^|^| GOTO ERROR >> !SCRIPT!
+  CLS
+  ECHO * Selected Platform *
+  ECHO !TARGET!
+  ECHO.
+  ECHO * Runtime^(s^)      *
+  TYPE OPTS.TXT
+  ECHO.
+  ECHO Do you want to add another runtime for this platform ?
+  CHOICE /C YN /M "Press Y for Yes, N for No."
+  IF "%ERRORLEVEL%" NEQ "2" ( GOTO START_SELECT_RUNTIME )
+  DEL OPTS.TXT
+  EXIT /B
 
-:ANALYSE_PATH
+:END_SELECT_RUNTIME
 
-DIR /B !CHOICE! > NUL
-IF %ERRORLEVEL% NEQ 0 (
-  ECHO *** Error ! Path !CHOICE! does not exist
-  GOTO SELECT_COMPILER
-)
-SET GNAT=!CHOICE!
-
-SET OLD_PWD=!CD!
-CD !CHOICE!\..\..
-IF %ERRORLEVEL% == 0 (
-  SET GNATROOT=!CD!
-)
-CD !OLD_PWD!
-:COMPILER_END
-
-REM We do not need GNATROOT.TXT anymore.
-DEL GNATROOT.TXT
-
-
-ECHO Using !GNAT! MAKE to compile AUnit
-
-
+CLS
 ECHO.
-ECHO *** INSTALLATION DIRECTORY ***
+ECHO *** NOW COMPILING AUNIT ***
+ECHO.
+CALL RUN.BAT || GOTO ERROR
+DEL RUN.BAT
+CLS
+ECHO.
+ECHO *** AUNIT COMPILED SUCCESSFULLY ***
 ECHO.
 ECHO Please enter AUnit base installation directory
-ECHO ** Press enter for installing in '!GNATROOT!':
+ECHO ** Press enter for installing in '%GPRBUILDROOT%':
 SET /P INSTALL=
-IF '%INSTALL%' == '' SET INSTALL=%GNATROOT%
 
-if NOT '%RUNTIME%' == '' GOTO SKIP_RUNTIME
-ECHO.
-ECHO *** RUN-TIME CONFIGURATION ***
-ECHO.
-ECHO Please enter the run-time you want to use when compiling AUnit:
-ECHO ** Press enter for default run-time:'
-SET /P RUNTIME=
-:SKIP_RUNTIME
-
-
-ECHO.
-ECHO *** AUNIT SUPPORT: EXCEPTIONS ***
-ECHO.
-:SELECT_EXCEPTION
-ECHO AUnit support for exceptions
-ECHO ** Choose 1 or 2, or press enter for default
-ECHO 1- Enable (default)
-ECHO 2- Disable
-SET CHOICE=
-SET /P CHOICE=
-SET SUPPORT_EXCEPTION=
-IF '!CHOICE!' == '' SET SUPPORT_EXCEPTION=yes
-IF '!CHOICE!' == '1' SET SUPPORT_EXCEPTION=yes
-IF '!CHOICE!' == '2' SET SUPPORT_EXCEPTION=no
-IF '!SUPPORT_EXCEPTION!' == '' (
-  ECHO.
-  ECHO * please type 1 or 2, or just press enter for default *
-  ECHO.
-  GOTO SELECT_EXCEPTION
-)
-
-ECHO.
-ECHO *** AUNIT SUPPORT: ADA.CALENDAR ***
-ECHO.
-:SELECT_CALENDAR
-ECHO AUnit support for Ada.Calendar
-ECHO ** Choose 1 or 2, or press enter for default
-ECHO 1- Enable (default)
-ECHO 2- Disable
-SET CHOICE=
-SET /P CHOICE=
-SET SUPPORT_CALENDAR=
-IF '!CHOICE!' == '' SET SUPPORT_CALENDAR=yes
-IF '!CHOICE!' == '1' SET SUPPORT_CALENDAR=yes
-IF '!CHOICE!' == '2' SET SUPPORT_CALENDAR=no
-IF '%SUPPORT_CALENDAR%' == '' (
-  ECHO.
-  ECHO * please type 1 or 2, or just press enter for default *
-  ECHO.
-  GOTO SELECT_CALENDAR
-)
-
-REM === end of user parameters. Do not change below this line
-
-:SETUP_VARS
-IF x%RUNTIME% EQU x (
-  SET ADA_FLAGS=
-) ELSE (
-  SET ADA_FLAGS=--RTS=%RUNTIME%
-)
-
-:SETUP
+IF '%INSTALL%' == '' SET INSTALL=%GPRBUILDROOT%
 SET I_GPR=%INSTALL%\lib\gnat
 SET I_TPL=%INSTALL%\share\examples\aunit
 SET I_DOC=%INSTALL%\share\doc\aunit
@@ -187,55 +212,25 @@ SET I_INC=%INSTALL%\include\aunit
 SET I_LIB=%INSTALL%\lib\aunit
 
 ECHO.
-ECHO **********************************
-ECHO *** BUILDING THE AUNIT LIBRARY ***
-ECHO **********************************
-ECHO.
-
-IF NOT %SUPPORT_EXCEPTION% EQU yes (
-  ECHO * no support for exceptions *
-  SET GPR_FLAGS_EXCEPTION=-XSUPPORT_EXCEPTION=no
-) ELSE (
-  ECHO AUnit set-up with exceptions support
-  SET GPR_FLAGS_EXCEPTION=-XSUPPORT_EXCEPTION=yes
-)
-IF NOT %SUPPORT_CALENDAR% EQU yes (
-  ECHO * no support for Ada.Calendar *
-  SET GPR_FLAGS_CALENDAR=-XSUPPORT_CALENDAR=no
-) ELSE (
-  ECHO AUnit set-up with Ada.Calendar support
-  SET GPR_FLAGS_CALENDAR=-XSUPPORT_CALENDAR=yes
-)
-SET GPR_FLAGS=!GPR_FLAGS_EXCEPTION! !GPR_FLAGS_CALENDAR!
-
-REM The following mkdirs are a workaround for missing gnatmake -p
-REM switch in versions prior to GNAT Pro 6.0.0
-MKDIR aunit\lib 2> NUL
-MKDIR aunit\obj 2> NUL
-
-ECHO %GNAT% make %ADA_FLAGS% -Paunit/aunit_build %GPR_FLAGS%
-%GNAT% make %ADA_FLAGS% -Paunit/aunit_build %GPR_FLAGS%
-
-:INSTALL
-ECHO.
 ECHO ************************
 ECHO *** INSTALLING AUNIT ***
 ECHO ************************
 ECHO.
+
+ECHO.
+ECHO uninstalling previous AUnit library in %INSTALL% if needed
+DEL /Q "%I_GPR%\aunit.gpr" 2> NUL
+DEL /Q "%I_TPL%\*.ad*" 2> NUL
+DEL /Q "%I_DOC%\*.*" 2> NUL
+RMDIR /Q/S "%I_INC%" 2> NUL
+RMDIR /Q/S "%I_LIB%" 2> NUL
+
 MKDIR "%I_DOC%" 2> NUL
 MKDIR "%I_GPR%" 2> NUL
 MKDIR "%I_TPL%" 2> NUL
 MKDIR "%I_PLG%" 2> NUL
 MKDIR "%I_LIB%" 2> NUL
 MKDIR "%I_INC%" 2> NUL
-
-ECHO.
-ECHO uninstalling previous AUnit library in %INSTALL% if needed
-DEL /Q "%I_GPR%\aunit.gpr"
-DEL /Q "%I_TPL%\*.ad*"
-DEL /Q "%I_DOC%\*.*"
-DEL /Q/F "%I_INC%\*.*"
-DEL /Q/F "%I_LIB%\*.*"
 
 ECHO.
 ECHO copying examples in %I_TPL%
@@ -255,19 +250,16 @@ COPY support\aunit.gpr "%I_GPR%" > NUL
 
 ECHO.
 ECHO copying AUnit source files in %I_INC%
-%GNAT% LIST -s -d -Paunit/aunit_build %GPR_FLAGS% | sort > files.txt
-SET PREV=
-FOR /F %%A IN ('TYPE files.txt') DO (
-  IF !PREV! NEQ %%A (
-    COPY "%%A" "%I_INC%" > NUL
-  )
-  SET PREV=%%A
-)
-DEL files.txt
+MKDIR "%I_INC%\framework" 2> NUL
+MKDIR "%I_INC%\reporters" 2> NUL
+MKDIR "%I_INC%\containers" 2> NUL
+XCOPY /S/Q /EXCLUDE:support\exclude.txt aunit\framework "%I_INC%\framework"   > NUL
+XCOPY /S/Q /EXCLUDE:support\exclude.txt aunit\reporters "%I_INC%\reporters"   > NUL
+XCOPY /S/Q /EXCLUDE:support\exclude.txt aunit\containers "%I_INC%\containers" > NUL
 
 ECHO.
 ECHO copying AUnit lib files in %I_LIB%
-COPY aunit\lib\*.* "%I_LIB%" > NUL
+XCOPY /S/Q aunit\lib\*.* "%I_LIB%" > NUL
 
 ECHO.
 ECHO *******************************************
@@ -276,4 +268,24 @@ ECHO *******************************************
 ECHO.
 ECHO Press enter to close this window
 SET /P FINISH=
+
+GOTO END
+
+:MISSINGGPRBUILD
+ECHO.
+ECHO GPRBUILD IS MISSING ON YOUR SYSTEM. PLEASE INSTALL IT FIRST !
+ECHO.
+ECHO.
+
+:ERROR
+ECHO.
+ECHO ******************************************************
+ECHO * !!! AN ERROR OCCURED DURING THIS SETUP PROCESS !!! *
+ECHO *              PLEASE TRY AGAIN.                     *
+ECHO *                                                    *
+ECHO *           AUNIT IS NOT INSTALLED                   *
+ECHO ******************************************************
+ECHO Type ^<Enter^> to terminate
+SET /P CHOICE=
+EXIT /B 1
 :END
