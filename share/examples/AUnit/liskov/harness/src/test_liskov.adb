ec0216868ca8b43bff723b3dd435f0afab521b11
Jerome Lambourg <lambourg@adacore.com>
2008-04-03 10:49:01 +0000

    Rename harness as test_liskov. This allows proper compilation on platforms  where executable do not have suffixes, as the previous name conflicted with  the directory name. Part of H201-024

--- share/examples/AUnit/liskov/harness/src/test_liskov.adb
+++ share/examples/AUnit/liskov/harness/src/test_liskov.adb
@@ -5,9 +5,9 @@ with AUnit.Reporter.Text;
 with AUnit.Run;
 with My_Suite; use My_Suite;
 
-procedure Harness is
+procedure Test_Liskov is
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
    Runner (Reporter);
-end Harness;
+end Test_Liskov;
