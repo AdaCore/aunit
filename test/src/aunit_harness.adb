6aaa691eaeaa89b818ff9d480621dcd26eb4b9ce
Jerome Lambourg <lambourg@adacore.com>
2009-06-15 09:53:39 +0000

    * makefile, aunit_tests.gpr: Provide a way to compile the test suite with the installed aunit library (as  opposed to the compiled one from the source repository).
    * aunit_harness.adb:
    Use the text output (simpler to analyse manually), and do not display the
     time (useless in this case).
    
    Part of I610-013

--- test/src/aunit_harness.adb
+++ test/src/aunit_harness.adb
@@ -1,11 +1,14 @@
-with AUnit.Reporter.XML;
+--
+--  Copyright (C) 2009, AdaCore
+--
+with AUnit.Reporter.Text;
 with Harness;
 
 --  with Last_Chance_Handler, Dummy_SS_Get;
 --  For ZFP only
 
 procedure AUnit_Harness is
-   Reporter : AUnit.Reporter.XML.XML_Reporter;
+   Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
-   Harness (Reporter);
+   Harness (Reporter, Timed => False);
 end AUnit_Harness;
