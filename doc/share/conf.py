c11404528e42feac0f146e0fae0770a7d90809c7
Nicolas Boulenguez <nicolas@debian.org>
2020-10-20 02:40:10 +0200

    Adapt doc/share/conf.py to sphinx4
    This syntax is recommended by sphinx3 and required by sphinx4.

--- doc/share/conf.py
+++ doc/share/conf.py
@@ -99,5 +99,5 @@ texinfo_documents = [
 
 
 def setup(app):
-    app.add_lexer('ada', ada_pygments.AdaLexer())
-    app.add_lexer('gpr', ada_pygments.GNATProjectLexer())
+    app.add_lexer('ada', ada_pygments.AdaLexer)
+    app.add_lexer('gpr', ada_pygments.GNATProjectLexer)
