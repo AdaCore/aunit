77a256f9fe136be1de945e6119bf3091599edb1a
Vasiliy Fofanov <fofanov@adacore.com>
2021-12-15 19:53:01 +0100

    Remove unnecessary with of ancestor, which causes warnings in recent GNAT.
    TN: UC15-089

--- examples/calculator/tests/operations-addition-test.ads
+++ examples/calculator/tests/operations-addition-test.ads
@@ -1,8 +1,7 @@
 --
---  Copyright (C) 2008, AdaCore
+--  Copyright (C) 2021, AdaCore
 --
 with Operations.Binary.Gen_Test;
-with Operations.Addition;
 with Operations.Addition_Test_Fixture; use Operations.Addition_Test_Fixture;
 package Operations.Addition.Test is new Operations.Addition.Gen_Test
   (Set_Up);
